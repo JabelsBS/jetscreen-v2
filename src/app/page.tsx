b192aa4d2950bee93f03f8e27e66598f0d1d9e17
JabelsBS <64330818+JabelsBS@users.noreply.github.com>
2025-07-31 08:38:34 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       title: "Flight",
       stat: statePlaneData?.callsign,
       fullWidth: true,
-      textSize: "text-sm",
+      textSize: "text-lg",
     },
   ];
 
