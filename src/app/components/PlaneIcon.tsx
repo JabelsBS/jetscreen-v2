e4430a1f5ad4f051fffb9e90939990055124022b
JabelsBS <64330818+JabelsBS@users.noreply.github.com>
2025-07-31 12:18:11 +0200

    Update PlaneIcon.tsx

--- src/app/components/PlaneIcon.tsx
+++ src/app/components/PlaneIcon.tsx
@@ -8,6 +8,7 @@ export const PlaneIcon = () => {
       height="150"
       viewBox="0 0 2501 2501"
       fill="white"
+      transform="rotate(90 1250.5 1250.5)"
     >
       <path
         fillRule="evenodd"
